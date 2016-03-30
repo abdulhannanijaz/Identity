e49496eab5f51f09d764eb921782975740524c9d
Abdul Hannan Ijaz <abdulhannanijaz@gmail.com>
2016-03-30 21:35:28 +0500

    Identity Manager Practice

---
+++ Identity/App_Start/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+
+namespace Identity.App_Start
+{
+
+        public class Startup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                app.UseCookieAuthentication(new CookieAuthenticationOptions
+                {
+                    AuthenticationType = "ApplicationCookie",
+                    LoginPath = new PathString("/sec/login")
+                });
+            }
+        }
+    
+}
